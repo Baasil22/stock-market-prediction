1dc4e96d771508453734fb9f940a9d7b799d69b8
Baasil22 <mohammedbaasil2004@gmail.com>
2025-11-04 15:11:41 +0530

    Updated minor change

--- app.py
+++ app.py
@@ -151,4 +151,4 @@ def download_file(filename):
     return send_file(os.path.join('static', filename), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
