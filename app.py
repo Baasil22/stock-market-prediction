53b72b4913b4d6abe78a1260d2952ea428522f51
Baasil22 <mohammedbaasil2004@gmail.com>
2025-11-04 15:31:16 +0530

    Updated minor change

--- app.py
+++ app.py
@@ -151,4 +151,5 @@ def download_file(filename):
     return send_file(os.path.join('static', filename), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    port = int(os.environ.get('PORT', 5000))  # <- Render provides PORT
+    app.run(host='0.0.0.0', port=port, debug=False)
